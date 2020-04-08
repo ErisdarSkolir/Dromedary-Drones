11eb31eca3a7521b9006094c71d0f2c67cd0065e
Luke <ldonmoyer@norrychristian.net>
2020-04-08 14:24:51 -0400

    Fixed crash

--- src/edu/gcc/gui/UiText.java
+++ src/edu/gcc/gui/UiText.java
@@ -4,7 +4,7 @@ import java.util.MissingResourceException;
 import java.util.ResourceBundle;
 
 public class UiText {
-	private static final String BUNDLE_NAME = "edu.gcc.gui.messages"; //$NON-NLS-1$
+	private static final String BUNDLE_NAME = "edu.gcc.gui.ui_text"; //$NON-NLS-1$
 
 	private static final ResourceBundle RESOURCE_BUNDLE = ResourceBundle.getBundle(BUNDLE_NAME);
 
