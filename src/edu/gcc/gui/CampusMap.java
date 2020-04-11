baefa377cbbf22aeb2610c078d003416a9d035dc
Luke <ldonmoyer@norrychristian.net>
2020-04-10 23:04:40 -0400

    Trying to interact with code

--- src/edu/gcc/gui/CampusMap.java
+++ src/edu/gcc/gui/CampusMap.java
@@ -1,66 +1,32 @@
 package edu.gcc.gui;
 
-import java.net.URL;
-import java.util.ResourceBundle;
-
 import edu.gcc.maplocation.Campus;
 import edu.gcc.maplocation.MapLocation;
 import edu.gcc.maplocation.MapLocationXml;
 import edu.gcc.maplocation.MapLocationXmlDao;
 import javafx.collections.ListChangeListener;
 import javafx.collections.ObservableList;
-import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
+import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.ScatterChart;
 import javafx.scene.chart.XYChart;
 import javafx.scene.chart.XYChart.Data;
 import javafx.scene.chart.XYChart.Series;
-import javafx.scene.control.Button;
-import javafx.scene.control.ComboBox;
 
-public class Overview implements Initializable{
+public class CampusMap {
+	// XML
 	private MapLocationXmlDao locationXml = MapLocationXml.getInstance();
-	
-	@FXML
-	private Button newCampusButton;
-	
-	@FXML
-	private ComboBox<Campus> campusDropdown;
-	
-	@FXML
-	private ScatterChart<Number, Number> campusMap;
+
+	// Map element
+	private ScatterChart<Number, Number> element;
+
 	// Map data
 	private ObservableList<XYChart.Data<Number, Number>> mapDropoffLocations;
 	private ObservableList<XYChart.Data<Number, Number>> mapPickupLocations;
-	
-	@FXML
-	protected void newCampusButtonClicked() {
-		System.out.println("New Campus button clicked");
-	}
-	
-	@FXML
-	protected void campusDropdownClicked() { 
-		System.out.println("Dropdown clicked");
+
+	public CampusMap() {
+		this.element = createMap();
 	}
 
-	@Override
-	public void initialize(URL location, ResourceBundle resources) {
-		/*// Pickup Points
-		Series<Number, Number> pickupLocationSeries = new Series<>();
-		pickupLocationSeries.setName("Shop Location");
-		mapPickupLocations = pickupLocationSeries.getData();
-
-		// Dropoff Points
-		Series<Number, Number> dropoffLocationSeries = new Series<>();
-		dropoffLocationSeries.setName("Delivery Points");
-		mapDropoffLocations = dropoffLocationSeries.getData();
-
-		campusMap.getData().add(pickupLocationSeries);
-		campusMap.getData().add(dropoffLocationSeries);
-		
-		setMapLocationData(new Campus("Test"));*/
-	}
-	
 	public void setMapLocationData(final Campus campus) {
 		if (campus == null)
 			return;
@@ -95,5 +61,33 @@ public class Overview implements Initializable{
 						.forEach(list::add);
 			}
 		};
+	}
+
+	// Creates a map
+	private ScatterChart<Number, Number> createMap() {
+		// Map
+		ScatterChart<Number, Number> result = new ScatterChart<>(new NumberAxis(), new NumberAxis());
+		result.getXAxis().setTickLabelsVisible(false);
+		result.getYAxis().setTickLabelsVisible(false);
+		result.setTitle("Campus Map");
+
+		// Pickup Points
+		Series<Number, Number> pickupLocationSeries = new Series<>();
+		pickupLocationSeries.setName("Shop Location");
+		mapPickupLocations = pickupLocationSeries.getData();
+
+		// Dropoff Points
+		Series<Number, Number> dropoffLocationSeries = new Series<>();
+		dropoffLocationSeries.setName("Delivery Points");
+		mapDropoffLocations = dropoffLocationSeries.getData();
+
+		result.getData().add(pickupLocationSeries);
+		result.getData().add(dropoffLocationSeries);
+
+		return result;
+	}
+
+	public ScatterChart<Number, Number> getElement() {
+		return element;
 	}
 }
