a3ed98d2775fb415eac50ebc47e01b67438d3e19
Luke <ldonmoyer@norrychristian.net>
2020-05-03 19:54:56 -0400

    Removed axes and title setting

--- src/edu/gcc/gui/Statistics.java
+++ src/edu/gcc/gui/Statistics.java
@@ -2,18 +2,13 @@ package edu.gcc.gui;
 
 import java.io.File;
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Optional;
 import java.util.ResourceBundle;
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutionException;
+
 import edu.gcc.results.Results;
-import edu.gcc.simulation.Simulation;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.chart.LineChart;
-import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.XYChart;
 import javafx.scene.control.ListView;
 import javafx.stage.FileChooser;
@@ -22,49 +17,21 @@ public class Statistics implements Initializable {
 	// TODO: add xml dao for run history stuff here
 
 	/*
-	 * Line Charts
-	 */
-
-	// Chart Axes
-	@FXML
-	private NumberAxis xAxis1 = new NumberAxis();
-	@FXML
-	private NumberAxis yAxis1 = new NumberAxis();
-	@FXML
-	private NumberAxis xAxis2 = new NumberAxis();
-	@FXML
-	private NumberAxis yAxis2 = new NumberAxis();
-	@FXML
-	private NumberAxis xAxis3 = new NumberAxis();
-	@FXML
-	private NumberAxis yAxis3 = new NumberAxis();
-
-	/*
 	 * LineCharts
 	 */
 	@FXML
-	private LineChart<Number, Number> chart_one = new LineChart<>(
-			xAxis1,
-			yAxis1
-
-	);
+	private LineChart<Number, Number> chart_one;
 	@FXML
-	private LineChart<Number, Number> chart_two = new LineChart<>(
-			xAxis2,
-			yAxis2
-	);
+	private LineChart<Number, Number> chart_two;
 	@FXML
-	private LineChart<Number, Number> chart_three = new LineChart<>(
-			xAxis3,
-			yAxis3
-	);
+	private LineChart<Number, Number> chart_three;
 
-	private XYChart.Series fifoSeries1 = new XYChart.Series();
-	private XYChart.Series knapsackSeries1 = new XYChart.Series();
-	private XYChart.Series fifoSeries2 = new XYChart.Series();
-	private XYChart.Series knapsackSeries2 = new XYChart.Series();
-	private XYChart.Series fifoSeries3 = new XYChart.Series();
-	private XYChart.Series knapsackSeries3 = new XYChart.Series();
+	private XYChart.Series<Number, Number> fifoSeries1 = new XYChart.Series<>();
+	private XYChart.Series<Number, Number> knapsackSeries1 = new XYChart.Series<>();
+	private XYChart.Series<Number, Number> fifoSeries2 = new XYChart.Series<>();
+	private XYChart.Series<Number, Number> knapsackSeries2 = new XYChart.Series<>();
+	private XYChart.Series<Number, Number> fifoSeries3 = new XYChart.Series<>();
+	private XYChart.Series<Number, Number> knapsackSeries3 = new XYChart.Series<>();
 
 	private int fifoCounter1 = 0;
 	private int knapsackCounter1 = 0;
@@ -183,31 +150,6 @@ public class Statistics implements Initializable {
 		windowBarController.backButtonEnableAndAction(
 			event -> Gui.getInstance().navigateTo("overview")
 		);
-		
-		xAxis1.setLabel("Sim Number");
-		yAxis1.setLabel("Average Delivery Time (minutes/order)"); // creating
-		// the chart
-		// final
-		chart_one.setTitle("Average Delivery Time");
-		fifoSeries1.setName("FIFO");
-		knapsackSeries1.setName("Knapsack");
-		
-		xAxis2.setLabel("Sim Number");
-		yAxis2.setLabel("Average Orders (Orders/Trip)"); // creating the chart
-		// final
-		chart_two.setTitle("Orders per Trip");
-
-		fifoSeries2.setName("FIFO");
-		knapsackSeries2.setName("Knapsack");
-		
-		xAxis3.setLabel("Sim Number");
-		yAxis3.setLabel("Average Distance (Distance/trip)"); // creating the
-		// chart final
-
-		chart_three.setTitle("Distance per Trip");
-
-		fifoSeries3.setName("FIFO");
-		knapsackSeries3.setName("Knapsack");
 		
 		chart_one.getData().add(fifoSeries1);
 		chart_one.getData().add(knapsackSeries1);
