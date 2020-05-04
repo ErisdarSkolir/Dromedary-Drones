c0acef8231905e2ce16ad2482a71c319fa1fd96f
Luke <ldonmoyer@norrychristian.net>
2020-05-03 22:10:03 -0400

    Document Main class

--- src/edu/gcc/main/Main.java
+++ src/edu/gcc/main/Main.java
@@ -3,11 +3,13 @@ package edu.gcc.main;
 import edu.gcc.gui.Gui;
 import javafx.application.Application;
 
+/**
+ * Main class for the Team Orange Comp 350 A Dromedary Drones Project.
+ * 
+ * @author Luke Donmoyer, Ethan Harvey, Lake Pry, Zack Orlaski
+ */
 public class Main {
 	public static void main(String[] args) {
-		//Thread thread = new Thread(() -> {
-			Application.launch(Gui.class, args);
-		//});
-		//thread.start();
+		Application.launch(Gui.class, args);
 	}
 }
